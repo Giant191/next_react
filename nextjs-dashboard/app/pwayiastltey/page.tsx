e61b02fdd36cb3b311e7ab215528b3976ebb9250
Wyatt Chang <dog414838@gmail.com>
2023-11-12 00:10:49 +0800

    modified:   nextjs-dashboard/app/pwayiastltey/page.tsx

--- nextjs-dashboard/app/pwayiastltey/page.tsx
+++ nextjs-dashboard/app/pwayiastltey/page.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
 import { lusitana } from '@/app/ui/fonts'
+import { Button } from '../ui/button';
+
+function sayHello() {
+    alert('You clicked me!');
+}
 
 export default function Page() {
+
     return (
         <main className="flex min-h-screen flex-col p-6">
-            <p>Paisley Wyatt</p>
+            <p>Paisley</p>
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
                 點開這個連結，還願意給我一點機會就點下載
             </p>
-            <button>下載</button>
+            <a href="nextjs-dashboard/app/pwayiastltey/S__241999875.jpg" target="_blank">
+                <Button> Link Button </Button>
+            </a>
+            <a download="S__241999875.jpg" target="_blank" href="nextjs-dashboard/app/pwayiastltey/S__241999875.jpg">
+                <Button> 下載 </Button>
+            </a>
         </main>
     );
 };
