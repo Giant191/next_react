2525ebc478157118ca393db580e757ce299ba637
Wyatt Chang <dog414838@gmail.com>
2023-11-22 21:43:46 +0800

    modified:   nextjs-dashboard/app/pwayiastltey/page.tsx

--- nextjs-dashboard/app/pwayiastltey/page.tsx
+++ nextjs-dashboard/app/pwayiastltey/page.tsx
@@ -15,13 +15,13 @@ export default async function Page() {
     return (
         <main className="flex min-h-screen flex-col p-6">
             <p></p>
-            <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            {/* <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
                 Paisley請使用電腦開啟這網頁<br></br>
                 進到這個頁面，願意給我一點機會，點擊下載
             </p>
             <div className="clipping-container">
                 <PortalExample />
-            </div>
+            </div> */}
             {/* <div>{latestInvoices}</div> */}
         </main>
     );
